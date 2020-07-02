cde2804b15adaeaf448b1787c3a58e9601e204e9
Marcelo Clares <msc.09.wylde@gmail.com>
2020-07-01 22:31:58 -0300

    Getting an UserById

--- src/main/java/com/example/demo/services/UserService.java
+++ src/main/java/com/example/demo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.User;
 import com.example.demo.repositories.UserRepository;
+import com.example.demo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +17,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findById(id).orElse(null);
+		if (user == null){
+			throw new ObjectNotFoundException("Object NOt Found!");
+		}
+		return user;
 	}
 }
