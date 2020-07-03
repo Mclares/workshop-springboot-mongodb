33c16f3908e0f4d6e120c7dcadfb613801a081e8
Marcelo Clares <msc.09.wylde@gmail.com>
2020-07-03 16:59:25 -0300

    Query with Multiple Criteria

--- src/main/java/com/example/demo/repositories/PostRepository.java
+++ src/main/java/com/example/demo/repositories/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repositories;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,9 +12,12 @@ import com.example.demo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	@Query(value = "{ 'title': { $regex: ?0, $options: 'i' } }")
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> findByTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ {date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 	
 }
